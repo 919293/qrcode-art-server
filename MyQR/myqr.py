c6b6150b4e94591a6488b49758ce7a071893291d
tiaod <tiaod@qq.com>
2018-06-13 00:20:59 +0800

    移除多余的边框

--- MyQR/myqr.py
+++ MyQR/myqr.py
@@ -3,7 +3,7 @@
 
 import os
 from MyQR.mylibs import theqrmodule
-from PIL import Image,ImageOps,ImageEnhance, ImageFilter
+from PIL import Image,ImageDraw,ImageOps,ImageEnhance, ImageFilter
 from MyQR.mylibs.constant import alig_location
 import tempfile
 
@@ -39,7 +39,17 @@ def combine(ver, qr_name):
             if not ((i in (18,19,20)) or (j in (18,19,20)) or (i<24 and j<24) or (i<24 and j>qr.size[1]-49) or (i>qr.size[0]-49 and j<24) or ((i,j) in aligs) or (i%3==1 and j%3==1)):
                 qr.putpixel((i+12,j+12), 0)
     # qr.resize((qr.size[0]*3, qr.size[1]*3)).save(qr_name)
-    qr = ImageOps.expand(qr.crop(qr.getbbox()),border=3,fill='white') #添加边界
+    qr = ImageOps.expand(qr.crop(qr.getbbox()),border=3,fill=0) #添加边界
+    d = ImageDraw.Draw(qr)
+    #填补马眼
+    x_end = qr.size[0]-1
+    y_end = qr.size[1]-1
+    d.rectangle([0,0,26,2],'white')# 左上
+    d.rectangle([0,0,2,26],'white')
+    d.rectangle([x_end, 0, x_end - 26,2],'white') #右上
+    d.rectangle([x_end, 0, x_end - 2,26],'white')
+    d.rectangle([0, y_end, 2,y_end-26],'white') #右下
+    d.rectangle([0, y_end, 26, y_end-2],'white')
     return qr
 
 def run(words, version=1, level='H', scale=1):
